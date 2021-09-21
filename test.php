05bcd5fd44a4193c19902b317ca887e330a52c44
musadiq012 <musadiq.tareen@hotmail.com>
2021-09-21 19:07:18 +0500

    Update test.php

--- test.php
+++ test.php
@@ -3,7 +3,7 @@
 * Plugin Name: Check Update from github test
 * Plugin URI: https://www.yourwebsiteurl.com/
 * Description: This is the very first plugin I ever created.
-* Version: 1.6
+* Version: 1.7
 * Author: Musadiq Mehmood 
 * Author URI: http://yourwebsiteurl.com/
 **/
